8ac5f4163d67e92750e5c6bd77a6996b39653367
thy <doanthy.vo@gmail.com>
2018-04-28 22:54:09 -0400

    added characters and staging area

---
+++ assets/javascript/game.js
@@ -0,0 +1,23 @@
+$(document).ready(function() {
+
+//Set Global Variables Here
+var userChar = "";
+
+$("#game").hide()
+$("#stage").show()
+
+//Set Functions Here
+
+//Call Functions Here
+
+
+$("#stage").on("click", ".char", function character(){
+    $("#userChar")
+    $("#stage").hide()
+    $("#game").show()
+})
+
+//pseudo
+//when a character is clicked, that char is userChar, all other char are enemy chars
+
+})
